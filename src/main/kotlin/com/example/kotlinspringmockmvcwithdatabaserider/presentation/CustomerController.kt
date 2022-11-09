c7f74ff13a9b4f610ea45867dcab26c1371a086b
Msksgm <msksgm3124@gmail.com>
2022-11-09 09:43:29 +0900

    feat: Customer 単一取得ユースケースから受け取った値をレスポンスに追加するように修正

--- src/main/kotlin/com/example/kotlinspringmockmvcwithdatabaserider/presentation/CustomerController.kt
+++ src/main/kotlin/com/example/kotlinspringmockmvcwithdatabaserider/presentation/CustomerController.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinspringmockmvcwithdatabaserider.presentation
 
+import com.example.kotlinspringmockmvcwithdatabaserider.usecase.GetCustomerUseCase
 import com.example.realworldkotlinspringbootjdbc.openapi.generated.controller.CustomerApi
 import com.example.realworldkotlinspringbootjdbc.openapi.generated.model.Customer
 import com.example.realworldkotlinspringbootjdbc.openapi.generated.model.SingleCustomerReponse
@@ -10,16 +11,18 @@ import org.springframework.web.bind.annotation.RestController
 /**
  * Customer API のルーティングコントローラー
  *
+ * @property getCustomerUseCase Customer 単一取得ユースケース
  */
 @RestController
-class CustomerController : CustomerApi {
+class CustomerController(val getCustomerUseCase: GetCustomerUseCase) : CustomerApi {
     override fun getCustomerById(id: Int): ResponseEntity<SingleCustomerReponse> {
+        val customer = getCustomerUseCase.execute(id)
         return ResponseEntity(
             SingleCustomerReponse(
                 Customer(
-                    id = id,
-                    firstName = "first_name",
-                    lastName = "last_name"
+                    id = customer.id,
+                    firstName = customer.firstName,
+                    lastName = customer.lastName
                 )
             ),
             HttpStatus.OK
